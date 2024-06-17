5c78af7b702dfe03ee335420e2e0b6b9cd994ef7
Nnanyereugo <nnajiwisdom1998@gmail.com>
2024-06-17 14:08:57 +0100

    imoemented the delete cartItem button functionality

--- delete.php
+++ delete.php
@@ -6,10 +6,24 @@ session_start();
 if(isset($_GET['prod_id'])){ //get the clicked item by its id
     $clickedProd_id = $_GET['prod_id'];
     echo "The item i want to delete is " .$clickedProd_id . "<br>"; 
-    print_r($_SESSION['cartList']);
+    // print_r($_SESSION['cartList']);
     
     // since we have the product id and the whole list in the session sgv, we can get the product by its id 
     // we can loop through the session sgv to find the item whose id matches the clickedProd_id
-    
+    foreach($_SESSION['cartList'] as $key => $product){
+        // when we find the product
+        if($product['id'] == $clickedProd_id){
+            print_r($product);
+            // we delete it
+            $_SESSION['cartList'] = array_filter($_SESSION['cartList'], function($item) use ($product) {
+                return $item !== $product;
+            });
+            
+        }
+        // then redirect page back to the cart.php
+        header("Location: cart.php");
+    }
+
+
     
 }
